4ddeac9d1a7dadcc266121458ecf515ffb8d808c
shidatouy <709994624@qq.com>
2021-07-02 15:03:04 +0800

    删除一些不常用依赖

--- app/src/main/java/com/example/baseutil/TestActivity.java
+++ app/src/main/java/com/example/baseutil/TestActivity.java
@@ -2,6 +2,7 @@ package com.example.baseutil;
 
 import com.example.baseutil.databinding.ActivityTestBinding;
 import com.base_util.util.BaseActivity;
+import com.zhy.http.okhttp.OkHttpUtils;
 
 public class TestActivity extends BaseActivity<ActivityTestBinding> {
     @Override
