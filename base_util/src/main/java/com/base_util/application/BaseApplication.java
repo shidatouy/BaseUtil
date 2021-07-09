4faef18318bc9abef8f38246e9b0f18c726d9558
shidatouy <709994624@qq.com>
2021-07-09 15:39:21 +0800

    删除

--- base_util/src/main/java/com/base_util/application/BaseApplication.java
+++ base_util/src/main/java/com/base_util/application/BaseApplication.java
@@ -19,22 +19,15 @@ import androidx.multidex.MultiDex;
  * Created by Administrator on 2016/5/13.
  */
 public class BaseApplication extends Application {
-    private static BaseApplication instance;
     public static Context context;
-
-    public static BaseApplication getInstance() {
-        return instance;
-    }
 
     @Override
     public void onCreate() {
-        instance = this;
         super.onCreate();
         //Gloading
         Gloading.initDefault(new GlobalAdapter());
         initDialog();//初始化V3dialog
         initMmkv();
-        dirFile();
     }
 
     @Override
@@ -53,19 +46,6 @@ public class BaseApplication extends Application {
         DialogSettings.init();
         DialogSettings.style = DialogSettings.STYLE.STYLE_IOS;
         DialogSettings.theme = DialogSettings.THEME.LIGHT;
-
-        //文件下载地址
-        ComData.filePath = Environment.getExternalStorageDirectory() + "/Android/data/" + this.getPackageName() + "/imageFile/";
-    }
-
-    /**
-     * 创建下载文件
-     */
-    private void dirFile() {
-        File file = new File(ComData.filePath);
-        if (!file.exists()) {
-            file.mkdirs();
-        }
     }
 
 }
